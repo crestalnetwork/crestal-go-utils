8b1b9c59e10d0250f0a1c3d825ac0b45b23f9154
Ruihua Wang <ruihua@crestal.network>
2024-07-20 00:45:09 +0800

    feat: big int to float 64

---
+++ xutils/number.go
@@ -0,0 +1,17 @@
+package xutils
+
+import (
+	"log/slog"
+	"math/big"
+)
+
+// BigIntE18ToFloat64 converts a big int to float64, the big int is in 1e18 scale
+func BigIntE18ToFloat64(bi *big.Int, log *slog.Logger) float64 {
+	bf := big.NewFloat(0).SetInt(bi)
+	f, acc := bf.Quo(bf, big.NewFloat(1e18)).Float64()
+	if acc != 0 {
+		log.Error("cannot convert big int to float, has accuracy", "accuracy", acc, "int", bi.String())
+		f = 0
+	}
+	return f
+}
