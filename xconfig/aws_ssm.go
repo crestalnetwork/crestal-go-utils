628ce541bc37d0e37848d241643ad7bd69a61eb5
Ruihua Wang <ruihua@crestal.network>
2024-07-20 11:00:53 +0800

    feat: aws ssm param store pagination

--- xconfig/aws_ssm.go
+++ xconfig/aws_ssm.go
@@ -22,6 +22,7 @@ type AwsSsmParamStore struct {
 // loadAwsSsmParamStore preload AWS SSM Param Store to memory
 func (l *loader) loadAwsSsmParamStore() error {
 	ctx := context.Background()
+	// aws client
 	if awsConfig == nil {
 		// try to get default aws config
 		cfg, err := config.LoadDefaultConfig(ctx)
@@ -31,20 +32,36 @@ func (l *loader) loadAwsSsmParamStore() error {
 		awsConfig = &cfg
 	}
 	client := ssm.NewFromConfig(*awsConfig)
+
+	// init
+	if l.AwsSsmParams == nil {
+		l.AwsSsmParams = make(map[string]string)
+	}
+
+	// first request
 	res, err := client.GetParametersByPath(ctx, &ssm.GetParametersByPathInput{
 		Path:           aws.String(l.AwsSsmPath),
 		WithDecryption: aws.Bool(true),
 	})
-	if err != nil {
-		return err
-	}
-	if l.AwsSsmParams == nil {
-		l.AwsSsmParams = make(map[string]string)
-	}
-	for _, param := range res.Parameters {
-		if param.Name != nil && param.Value != nil {
-			name := strings.TrimPrefix(*param.Name, l.AwsSsmPath+"/")
-			l.AwsSsmParams[name] = *param.Value
+	// process then pagination
+	for {
+		if err != nil {
+			return err
+		}
+		for _, param := range res.Parameters {
+			if param.Name != nil && param.Value != nil {
+				name := strings.TrimPrefix(*param.Name, l.AwsSsmPath+"/")
+				l.AwsSsmParams[name] = *param.Value
+			}
+		}
+		if res.NextToken != nil {
+			res, err = client.GetParametersByPath(ctx, &ssm.GetParametersByPathInput{
+				Path:           aws.String(l.AwsSsmPath),
+				WithDecryption: aws.Bool(true),
+				NextToken:      res.NextToken,
+			})
+		} else {
+			break
 		}
 	}
 
